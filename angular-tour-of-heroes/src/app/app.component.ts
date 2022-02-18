29e9b1edf8bd145d67eb967baac9ff8560a31fd6
Nanask <ybhag1005@daum.net>
2022-02-18 17:18:19 +0900

    히어로 에디터 만들기

---
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  name = '지금 Angular 튜토리얼을 진행중이예요!';
+}
---
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+// Angular 구성요소나 라이브러리를 조합하기 위한 것들이 들어있는 메타데이터?
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
