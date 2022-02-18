a93ae399d34213875c67199884d1b2581a239bdd
Nanask <ybhag1005@daum.net>
2022-02-18 18:15:52 +0900

    목록 표시하기

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
 import { Hero } from './../hero';
-import { HEROES } from './../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +9,22 @@ import { HEROES } from './../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  // heroes = HEROES;
+  
+  heroes = HEROES;
 
-   hero: Hero = {
-    id: 1,
-    name: 'windstorm',
-  };
+  //  hero: Hero = {
+  //   id: 1,
+  //   name: 'windstorm',
+  // };
 
-  // hero = 'Windstorm';
-  // constructor() {}
+  // hero가 클릭됐을때 발생되는 함수, 어플리케이션을 처음 작동했을 때는 hero가 선택되지 않았기 때문에
+  // selectedHero에 값을 할당하지 않는다.
+  selectedHero? : Hero;
+  onSelect(hero:Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor() {}
 
   ngOnInit(): void {}
 }
